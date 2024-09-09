81348840d87802812471cca05b74cf0a216d6308
NIMMY04 <156294603+NIMMY04@users.noreply.github.com>
2024-09-09 18:43:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function getFormvalue(event) {
+	event.preventDefault();
     var Fname=document.querySelector('input["fname"]').value;
 	var Lname=document.querySelector('input["lname"]').value;
 	alert(Fname +" " +Lname);}
